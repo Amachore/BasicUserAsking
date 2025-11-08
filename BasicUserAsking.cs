d0192e2cf5c3f44e52c5688f3a736e468a573f61
Amachore <157425252+Amachore@users.noreply.github.com>
2025-11-09 00:48:55 +0800

    Add files via upload

---
+++ BasicUserAsking.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string welcomeMessage = $"Hi! Welcome to Vaughn's Contact Tracing Program. \nWrite the following details: \nName: ";
+            Console.Write(welcomeMessage);
+            string Name = Console.ReadLine();
+            Console.Write("Number/email: ");
+            string Contact = Console.ReadLine();
+            Console.Write("Address: ");
+            string Address = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor. Green;
+            Console.WriteLine("\nWell done. Ingat!:)");
+            Console.ResetColor();
+        }
+    }
+}
